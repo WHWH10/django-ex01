cc97662a86839995b5a247b4a8c04306202ed717
WHWH10 <eyet010@gmail.com>
2021-07-05 12:29:10 +0900

    CreateView/UpdateView/DeleteView/DetailView

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from accountapp.models import HelloWorld
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from accountapp.forms import AccountUpdateForm
 
 # Create your views here.
 def hello_world(request):
@@ -26,3 +30,34 @@ def hello_world(request):
         )
     # return render(request, "base.html")
     # return HttpResponse("Hello World")
+
+
+# Class Based View
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    # resverse_lazy : Class Based View 사용함
+    # reverse : Function Based View 사용함
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = "accountapp/create.html"
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = "target_user"
+    template_name = "accountapp/detail.html"
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    context_object_name = "target_user"
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = "accountapp/update.html"
+
+
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = "target_user"
+    success_url = reverse_lazy("accountapp:login")
+    template_name = "accountapp/delete.html"
--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ, os
+from django.urls import reverse_lazy
 
 env = environ.Env(DEBUG=(bool, False))
 
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accountapp",
+    "bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -110,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
 
 USE_I18N = True
 
@@ -129,6 +131,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy("accountapp:hello_world")
+LOGOUT_REDIRECT_URL = reverse_lazy("accountapp:login")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
